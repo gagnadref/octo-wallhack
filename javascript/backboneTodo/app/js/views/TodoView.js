85f0715f343a0b6d6990e5d3dcc30a4915a63e77
Paul Dib <paul.dib@orange.fr>
2014-03-27 10:44:17 +0100

    Removed the ugly HTML from JS.

--- javascript/backboneTodo/app/js/views/TodoView.js
+++ javascript/backboneTodo/app/js/views/TodoView.js
@@ -24,11 +24,9 @@ return Backbone.View.extend({
    },
    beginEdit: function beginEdit(event) {
 		event.preventDefault();
-		var $target = $(event.target);
-		var currentText = $target.text();
-		$target.remove();
-		var $input = createTitleInput(currentText);
-		this.$el.append($input);
+		this.isEditing = true;
+		this.render();
+		var $input = $("input[data-selection]", this.$el);
 		focusAndSelectAll($input);
    },
    saveTodo: function saveTodo(event){
@@ -36,6 +34,7 @@ return Backbone.View.extend({
 		var $target = $(event.target);
 		var currentText = $target.val();
 		this.model.set('title',currentText);
+		this.isEditing = false;
 		this.render();
    },
    handleEnterKeypress: function handleEnterKeypress(event) {
@@ -46,7 +45,7 @@ return Backbone.View.extend({
 		this.saveTodo(event);
    },
    render: function renderTodoView(){
-		this.$el.html(this.template(this.model.toJSON()));
+		this.$el.html(this.template(_.extend(this.model.toJSON(), {isEditing: this.isEditing})));
 		return this;
    }
 });
